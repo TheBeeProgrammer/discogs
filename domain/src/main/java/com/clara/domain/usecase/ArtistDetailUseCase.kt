cc86cd2052d9b768c111ab4b6181cf6d36295b52
renato <renatogc1996@gmail.com>
2025-07-20 02:37:03 -0500

    Add: KDoc to ArtistDetailUseCase

--- domain/src/main/java/com/clara/domain/usecase/ArtistDetailUseCase.kt
+++ domain/src/main/java/com/clara/domain/usecase/ArtistDetailUseCase.kt
@@ -7,6 +7,14 @@ import com.clara.domain.usecase.model.UseCaseResult
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * Use case for fetching the details of a specific artist.
+ *
+ * This class encapsulates the business logic for retrieving artist details.
+ * It interacts with the [ArtistDetailRepository] to get the data.
+ *
+ * @param repository The repository responsible for fetching artist details.
+ */
 class ArtistDetailUseCase @Inject constructor(
     private val repository: ArtistDetailRepository
 ) : ExecutableUseCase<Int, UseCaseResult<Flow<ArtistDetail>>> {
