359ea8dfe7542d6d4f6496bd77375cf5fa858355
renato <renatogc1996@gmail.com>
2025-07-20 08:37:31 -0500

    feat: Add testing dependencies for unit and instrumented tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,6 @@ detekt {
 android {
     namespace = "com.clara.clarachallenge"
     compileSdk = 36
-
     defaultConfig {
         applicationId = "com.clara.clarachallenge"
         minSdk = 25
@@ -68,13 +67,6 @@ dependencies {
     implementation(libs.androidx.paging.common.android)
     implementation(libs.androidx.paging.compose)
 
-    // --- Testing ---
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-
     // --- Debug ---
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
@@ -82,6 +74,23 @@ dependencies {
     // --- Dependency Injection (Hilt) ---
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+
+    // --- Testing ---
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.turbine)
+    testImplementation(libs.truth)
+    // Hilt testing dependencies
+    testImplementation(libs.hilt.android.testing)
+    kspTest(libs.hilt.android.compiler)
 
     //  Domain Layer
     implementation(project(":domain"))
