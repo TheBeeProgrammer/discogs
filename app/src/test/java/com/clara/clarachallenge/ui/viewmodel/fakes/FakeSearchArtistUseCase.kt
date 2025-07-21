1fb0ea256533d9ed16fd090df0b78071b5b1b696
renato <renatogc1996@gmail.com>
2025-07-21 13:57:21 -0500

    Docs: Add KDocs to fake use cases

--- app/src/test/java/com/clara/clarachallenge/ui/viewmodel/fakes/FakeSearchArtistUseCase.kt
+++ app/src/test/java/com/clara/clarachallenge/ui/viewmodel/fakes/FakeSearchArtistUseCase.kt
@@ -6,6 +6,14 @@ import com.clara.domain.usecase.base.ExecutableUseCase
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
+/**
+ * Fake implementation of [ExecutableUseCase] for testing purposes.
+ * This class simulates the behavior of searching for artists.
+ *
+ * @property lastReceivedQuery The last query received by the use case.
+ * @property searchResult The flow of [PagingData] representing the search results.
+ * @property callCount The number of times the use case has been invoked.
+ */
 class FakeSearchArtistUseCase : ExecutableUseCase<String, Flow<PagingData<Artist>>> {
     var lastReceivedQuery: String = ""
     var searchResult: Flow<PagingData<Artist>> = flowOf(PagingData.empty())
